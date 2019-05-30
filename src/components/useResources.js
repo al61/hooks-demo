ced55c84a4fd491765631d85134e22af5786dd46
Al Hammad <al.hammad@bluescape.com>
2019-05-30 10:02:54 -0700

    Add comments

--- src/components/useResources.js
+++ src/components/useResources.js
@@ -18,3 +18,10 @@ const useResources = resource => {
 };
 
 export default useResources;
+
+/*
+  - Traditionally in React, we’ve had two popular ways to share stateful logic between 
+    components: render props and higher-order components. We now have Hooks to
+    solve many of the same problems without forcing you to add more components to the tree.
+  - A custom Hook is a JavaScript function whose name starts with ”use” and that may call other Hooks
+*/
